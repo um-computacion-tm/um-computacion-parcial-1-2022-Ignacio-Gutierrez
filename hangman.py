58eccfdd26c088c8ef5482acd9fe6be3ea5eb31a
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2022-06-14 10:47:01 -0300

    show

---
+++ hangman.py
@@ -0,0 +1,21 @@
+class Hangman:
+
+    def life(self):
+        self.life = 5
+
+
+    def set_word(self, word):
+        self.set_word = word
+
+    def playing(set_word):
+        word = set_word
+
+    def assign(self, letter):
+        self.letter = letter
+    
+    def show(self):
+        return('Lifes: 5 '+'- '+'Word: '+ (len(self.set_word)*'_ '))        
+
+    def life():
+        pass
+
